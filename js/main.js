117c7109c576c8efc17c5a0fbfc7258d3276e567
vrubliuk <vrubliuk@gmail.com>
2018-01-08 20:58:21 +0200

    Add attractions section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 document.getElementById("header-button-menu").onclick = () => {
     let navigationLinks = document.getElementById("header-links");
     let buttonMenuIcon = document.getElementById("header-button-menu-icon");
+
+
     if (navigationLinks.className === "header-links") {
         navigationLinks.className += " responsive";
         buttonMenuIcon.setAttribute("data-icon", "angle-up");
@@ -13,10 +15,30 @@ document.getElementById("header-button-menu").onclick = () => {
     }
 }
 
-// document.body.onscroll = () => {
-//     if (Number(document.body.scrollHeight) > 0) {
-//        document.getElementsByTagName("header")[0].style.backgroundColor = "red";
-//     } else {
-//         document.getElementsByTagName("header")[0].style.backgroundColor = "transparent";
-//     }
-// }
+
+
+
+
+window.onscroll = () => {
+    let header = document.getElementsByTagName("header")[0];
+    let logo = document.getElementById('website-logo');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //    header.style.backgroundColor = "#1f2336";
+       header.style.backgroundColor = "rgba(0, 0,0, 0.75)";
+        // header.style.padding = "10px 0";
+  
+
+
+      } else {
+        header.style.backgroundColor = "";
+        // header.style.padding = "";
+   
+      }
+
+
+    // if (Number(document.body.scrollHeight) > 0) {
+    //    document.getElementsByTagName("header")[0].style.backgroundColor = "red";
+    // } else {
+    //     document.getElementsByTagName("header")[0].style.backgroundColor = "transparent";
+    // }
+}
