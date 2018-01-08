6aca6aa9b65f053df9eccc46af54028e92248b5a
vrubliuk <vrubliuk@gmail.com>
2018-01-09 00:53:27 +0200

    Finish flights section

--- js/main.js
+++ js/main.js
@@ -1,44 +1,48 @@
 'use strict';
+$(document).ready(function () {
+    // SMOOTH SCROLLING ANCHOR LINKS
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+    // HEADER
+    document.getElementById("header-button-menu").onclick = () => {
+        let navigationLinks = document.getElementById("header-links");
+        let buttonMenuIcon = document.getElementById("header-button-menu-icon");
 
-// HEADER
-document.getElementById("header-button-menu").onclick = () => {
-    let navigationLinks = document.getElementById("header-links");
-    let buttonMenuIcon = document.getElementById("header-button-menu-icon");
 
-
-    if (navigationLinks.className === "header-links") {
-        navigationLinks.className += " responsive";
-        buttonMenuIcon.setAttribute("data-icon", "angle-up");
-    } else {
-        navigationLinks.className = "header-links";
-        buttonMenuIcon.setAttribute("data-icon", "angle-down");
+        if (navigationLinks.className === "header-links") {
+            navigationLinks.className += " responsive";
+            buttonMenuIcon.setAttribute("data-icon", "angle-up");
+        } else {
+            navigationLinks.className = "header-links";
+            buttonMenuIcon.setAttribute("data-icon", "angle-down");
+        }
     }
-}
+
+    window.onscroll = () => {
+        let header = document.getElementsByTagName("header")[0];
+        let logo = document.getElementById('website-logo');
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            header.style.backgroundColor = "rgba(0, 0,0, 0.75)";
+        } else {
+            header.style.backgroundColor = "";
+        }
+    }
+
+    // CUSTOM SCROLLBAR
+    // let ps = new PerfectScrollbar('#table-container');
 
 
 
 
 
-window.onscroll = () => {
-    let header = document.getElementsByTagName("header")[0];
-    let logo = document.getElementById('website-logo');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //    header.style.backgroundColor = "#1f2336";
-       header.style.backgroundColor = "rgba(0, 0,0, 0.75)";
-        // header.style.padding = "10px 0";
-  
 
-
-      } else {
-        header.style.backgroundColor = "";
-        // header.style.padding = "";
-   
-      }
-
-
-    // if (Number(document.body.scrollHeight) > 0) {
-    //    document.getElementsByTagName("header")[0].style.backgroundColor = "red";
-    // } else {
-    //     document.getElementsByTagName("header")[0].style.backgroundColor = "transparent";
-    // }
-}
+});
